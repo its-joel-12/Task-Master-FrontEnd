8cd8e4efe171b2ec770f0ddc412c5577b47df510
Joel Oliveira <joel80851@gmail.com>
2024-05-07 18:17:36 +0530

    ...

---
+++ Js/EmployeeById.js
@@ -0,0 +1,13 @@
+const searchEmp = document.getElementById("searchEmp");
+const search_emp_by_id = document.getElementById("search_emp_by_id");
+
+// OPEN SEARCH_EMP_BY_ID MODAL
+function openSearchEmployeeByIdModal() {
+    search_emp_by_id.innerHTML = `
+          
+         hello
+          
+          `;
+  }
+
+  searchEmp.addEventListener("click", openSearchEmployeeByIdModal);
